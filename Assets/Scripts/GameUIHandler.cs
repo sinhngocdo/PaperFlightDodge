e6c8445e2b0aa6d8583a352d124e664225552c39
sinhngocdo <sinhngocdo@gmail.com>
2023-07-04 21:46:38 +0700

    Update Game UI

--- Assets/Scripts/GameUIHandler.cs
+++ Assets/Scripts/GameUIHandler.cs
@@ -1,15 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameUIHandler : MonoBehaviour
 {
     internal static bool startGame = false;
     [SerializeField] private PlayerController pc = null;
     [SerializeField] private EnemySpawner es = null;
+    [SerializeField] private TextMeshProUGUI bestTimeText = null;
+
     // Start is called before the first frame update
     void Start()
     {
+        bestTimeText.SetText("Best Time: " + (PlayerPrefs.GetInt("Minute").ToString().Length > 1 ? PlayerPrefs.GetInt("Minute").ToString() : "0" + PlayerPrefs.GetInt("Minute").ToString() + ":" + (PlayerPrefs.GetInt("Seconds").ToString().Length > 1 ? PlayerPrefs.GetInt("Seconds").ToString() : "0" + PlayerPrefs.GetInt("Seconds")).ToString()));
         startGame = false;
     }
 
@@ -24,5 +29,10 @@ public class GameUIHandler : MonoBehaviour
         startGame = true;
         pc.enabled = true;
         es.enabled = true;
+    }
+
+    public void RetryBtn()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -6,6 +6,9 @@ using TMPro;
 public class LevelManager : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI gameTimeTxt = null;
+    [SerializeField] private GameObject gamePlayUI = null;
+    [SerializeField] private GameObject gameOverUI = null;
+    [SerializeField] private TextMeshProUGUI gameOverText = null;
 
     private int minute = 0;
     private float seconds = 0f;
@@ -30,6 +33,25 @@ public class LevelManager : MonoBehaviour
             }
             gameTimeTxt.SetText(minute.ToString().Length>1?minute.ToString():"0" + minute.ToString() + ":" + (((int)seconds).ToString().Length > 1 ? ((int)seconds).ToString() : "0" + ((int)seconds).ToString()));
 
+        }
+        else
+        {
+            gameOverUI.SetActive(true);
+            gamePlayUI.SetActive(false);
+            gameOverText.SetText("Time: " + gameTimeTxt.GetParsedText());
+            int bestMinute = PlayerPrefs.GetInt("Minute");
+            int bestSeconds = PlayerPrefs.GetInt("Seconds");
+
+            if (minute > bestMinute)
+            {
+                PlayerPrefs.SetInt("Minute", minute);
+                PlayerPrefs.SetInt("Seconds", (int)seconds);
+            }
+            else if(minute >= bestMinute && seconds>bestSeconds)
+            {
+                PlayerPrefs.SetInt("Minute", minute);
+                PlayerPrefs.SetInt("Seconds", (int)seconds);
+            }
         }
     }
 }
